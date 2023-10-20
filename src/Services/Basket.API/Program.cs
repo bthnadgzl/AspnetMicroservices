1630ff360fd5d0b34059ac5b28463d93859940df
bthnadgzl <batuhanadigzl@gmail.com>
2023-10-20 17:00:18 +0300

    End Of CatalogApi

--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,6 +1,3 @@
-using Catalog.API.Repositories.impl;
-using Catalog.API.Repository;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,9 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICatalogContext, CatalogContext>();
-builder.Services.AddScoped<IProductService, ProductService>();
-
 
 var app = builder.Build();
 
@@ -23,6 +17,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
